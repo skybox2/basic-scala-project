3b1a69f966a3c6910d3125412fcfb2b5c2060fb2
skylarbrannon <skylarbrannon@gmail.com>
2017-04-11 21:13:33 -0700

    Updated pom.xml and added basic tests

--- src/test/scala/com/skylar/basicscalaproject/ApplicationTest.scala
+++ src/test/scala/com/skylar/basicscalaproject/ApplicationTest.scala
@@ -1,8 +1,23 @@
 package com.skylar.basicscalaproject
 
+import org.scalatest.FunSuite
+import org.scalatest.BeforeAndAfter
+
 /**
   * Created by skylar on 4/11/17.
   */
-class ApplicationTest {
+class ApplicationTest extends FunSuite with BeforeAndAfter {
+
+  before{
+    //set up variables
+  }
+
+  test("dummyTest") {
+    assert(true)
+  }
+
+  after {
+    //tear down variables
+  }
 
 }
